5a6b69b14d799957558e3fa249ee002411d063b5
Plofland <ploflan@gmail.com>
2021-05-18 14:41:59 -0400

    Added skilled badges

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../styles/components/Skills.scss';
 
-
 export default function Skills() {
 	return (
 		<div className="skillsSection">
@@ -9,8 +8,124 @@ export default function Skills() {
 				<h2>Skills</h2>
 			</div>
 			<div className="skills">
-				<div className="proficientSkills">😎</div>
-				<div className="experiencedSkills">🤩</div>
+				<div className="proficientSkillsSection">
+					<h3>Proficient</h3>
+					<div>
+						<img
+							src="https://img.shields.io/badge/-HTML5-61DAFB?logo=html5&logoColor=red&style=plastic"
+							alt="HTML badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-CSS3-61DAFB?logo=css3&logoColor=1572B6&style=plastic"
+							alt="CSS badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-ReactJS-61DAFB?logo=react&logoColor=white&style=plastic"
+							alt="React badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-JavaScript-61DAFB?logo=javascript&logoColor=F7DF1E&style=plastic"
+							alt="JavaScript badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-Redux-61DAFB?logo=redux&logoColor=764ABC&style=plastic"
+							alt="Redux badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-React--Router-61DAFB?logo=react%20router&logoColor=CA4245&style=plastic"
+							alt="React-Router badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-NPM-61DAFB?logo=npm&logoColor=white&style=plastic"
+							alt="NPM badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-Figma-61DAFB?logo=figma&logoColor=F24E1E&style=plastic"
+							alt="Figma badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-NodeJS-61DAFB?logo=node-dot-js&logoColor=339933&style=plastic"
+							alt="NodeJS badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-Express-61DAFB?logo=express&logoColor=000000&style=plastic"
+							alt="Express badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-Git-61DAFB?logo=git&logoColor=F05032&style=plastic"
+							alt="Git badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-GitHub-61DAFB?logo=github&logoColor=181717&style=plastic"
+							alt="GitHub badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-PostgreSQL-61DAFB?logo=postgresql&logoColor=336791&style=plastic"
+							alt="PostgreSQL badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-SQLite-61DAFB?logo=sqlite&logoColor=003B57&style=plastic"
+							alt="SQLite badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-Postman-61DAFB?logo=postman&logoColor=FF6C37&style=plastic"
+							alt="Postman badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-Styled--Components-61DAFB?logo=styled-components&logoColor=DB7093&style=plastic"
+							alt="Styled Components badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-Cypress-61DAFB?logo=cypress&logoColor=17202C&style=plastic"
+							alt="Cypress badge"
+						/>
+					</div>
+				</div>
+				<div className="experiencedSkillsSection">
+					<h3>Experienced</h3>
+					<div>
+						<img
+							src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=html5&logoColor=red&style=plastic"
+							alt="HTML badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=html5&logoColor=red&style=plastic"
+							alt="HTML badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=html5&logoColor=red&style=plastic"
+							alt="HTML badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=html5&logoColor=red&style=plastic"
+							alt="HTML badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=html5&logoColor=red&style=plastic"
+							alt="HTML badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=html5&logoColor=red&style=plastic"
+							alt="HTML badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=html5&logoColor=red&style=plastic"
+							alt="HTML badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=html5&logoColor=red&style=plastic"
+							alt="HTML badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=html5&logoColor=red&style=plastic"
+							alt="HTML badge"
+						/>
+						<img
+							src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=html5&logoColor=red&style=plastic"
+							alt="HTML badge"
+						/>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
