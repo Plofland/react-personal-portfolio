cd5f52cfe6003b03c19ee09f1db0eb3efd5c43a9
Plofland <ploflan@gmail.com>
2021-05-19 18:34:36 -0400

    All links now take the user offsite and button shading tweeked

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,16 @@ import React from 'react';
 import '../styles/components/Footer.scss';
 
 export default function Footer() {
-  return (
-    <div className="footer">
-      <p>Graphic image belongs to Marina Rakhimova</p>
-      <a href="https://dribbble.com/MarinaMakoto">
-        <p>Find more of her work here</p>
-      </a>
-    </div>
-  );
+	return (
+		<div className="footer">
+			<p>Graphic image belongs to Marina Rakhimova</p>
+			<a
+				href="https://dribbble.com/MarinaMakoto"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<p>Find more of her work here</p>
+			</a>
+		</div>
+	);
 }
