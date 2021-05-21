73eb11f0c20d5e7b1aab8f201255480855fbfde9
Plofland <ploflan@gmail.com>
2021-05-21 16:07:45 -0400

    AboutMe customized

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,23 +11,17 @@ export default function About() {
       <div className="AboutMeInner">
         <div className="selfDescription">
           <p>
-            I worked remotely to collaborate with teams
+            I worked remotely to collaborate with teams to
             produce viable web development projects within
             Lambda School's system. This gave me the chance
             to experience work environments while learning
-            web development.
+            web development tools and technology.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua.
-            Quis imperdiet massa tincidunt nunc pulvinar
-            sapien et. Gravida in fermentum et sollicitudin
-            ac orci. Aliquet bibendum enim facilisis gravida
-            neque convallis a cras. Blandit libero volutpat
-            sed cras ornare arcu dui vivamus arcu. Amet
-            mauris commodo quis imperdiet massa tincidunt
-            nunc pulvinar.
+            I enjoy overcoming obstacles and seeing a project from conception to deployment. I'm capable of working on both the front and back end but I have an affinity for UI/UX design. I always love learning anything about software engineering whether it's a tool, framework, workflow enhancement or a new language. I particularly relish working in a group but am adept at working independently to find my own solutions.
+          </p>
+          <p>
+            <u>Life outside of code:</u><p> On my free time I love cooking, camping, hiking, backpacking, photography or spending the entire day at the beach.</p>
           </p>
         </div>
         <div id="image">
