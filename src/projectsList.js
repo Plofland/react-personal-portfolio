109b1a20f4df25b2f3e213117f7265111ecef74c
Peter <ploflan@gmail.com>
2021-05-23 18:57:24 -0400

    LAN project added to Project Section

--- src/projectsList.js
+++ src/projectsList.js
@@ -1,39 +1,36 @@
 import hkLanding from './assets/product-preview.png';
-import tribute from './assets/TributePagePreview.png';
 import todoApp from './assets/todoReactApp.png';
 import coMake from './assets/co-make.png';
+import LAN from './assets/LAN.jpg';
 
 const projectsList = [
   {
     id: 0,
+    projectName: 'Lambda Alumni Association',
+    hrefLink: 'https://a.lambdaalumninetwork.dev/welcome',
+    imgSrc: LAN,
+    imgAlt: 'Lambda Alumni Association project'
+  },
+  {
+    id: 1,
     projectName: 'Co-Make',
     hrefLink: 'https://co-make-tt33.herokuapp.com/',
     imgSrc: coMake,
     imgAlt: 'Co-Make project'
   },
   {
-    id: 1,
+    id: 2,
     projectName: 'ToDo Application',
     hrefLink: 'https://my-very-own-todo-app.netlify.app/',
-    // imgSrc: '.assets/todoReactApp.png',
     imgSrc: todoApp,
     imgAlt: 'ToDo React App'
   },
   {
-    id: 2,
+    id: 3,
     projectName: 'Product Landing Page',
     hrefLink: 'https://hkproductlandingpage.netlify.app/',
-    // imgSrc: './assets/product-preview.png',
     imgSrc: hkLanding,
     imgAlt: 'Product landing page project'
-  },
-  {
-    id: 3,
-    projectName: 'Tribute Page',
-    hrefLink: 'https://codepen.io/plofland/full/VwaYVEv',
-    // imgSrc: './assets/TributePagePreview.png',
-    imgSrc: tribute,
-    imgAlt: 'Tribute page to MC Escher project'
   },
 ];
 
