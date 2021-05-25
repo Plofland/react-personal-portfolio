4a22daa015a3a4946c6ba6c34a14ec0d5a8c57f7
Peter <ploflan@gmail.com>
2021-05-25 15:53:49 -0400

    AboutMe bust shot and intro graphic optimized

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/components/AboutMe.scss';
 import bustShot from '../assets/selfBustShot.jpg';
+import bustShotMobile from '../assets/selfBustShotMobile.jpg';
 
 export default function About() {
   return (
@@ -18,17 +19,39 @@ export default function About() {
             web development tools and technology.
           </p>
           <p>
-            I enjoy overcoming obstacles and seeing a project from conception to deployment. I'm capable of working on both the front and back end but I have an affinity for UI/UX design. I always love learning anything about software engineering whether it's a tool, framework, workflow enhancement or a new language. I particularly relish working in a group but am adept at working independently to find my own solutions.
+            I enjoy overcoming obstacles and seeing a
+            project from conception to deployment. I'm
+            capable of working on both the front and back
+            end but I have an affinity for UI/UX design. I
+            always love learning anything about software
+            engineering whether it's a tool, framework,
+            workflow enhancement or a new language. I
+            particularly relish working in a group but am
+            adept at working independently to find my own
+            solutions.
           </p>
           <p>
-            <u>Life outside of code:</u><p> On my free time I love cooking, camping, hiking, backpacking, photography or spending the entire day at the beach.</p>
+            <u>Life outside of code:</u>
+            <p>
+              {' '}
+              On my free time I love cooking, camping,
+              hiking, backpacking, photography or spending
+              the entire day at the beach.
+            </p>
           </p>
         </div>
         <div id="image">
-          <img
-            src={bustShot}
-            alt="Peter Lofland bust shot"
-          />
+          {window.innerWidth <= 414 ? (
+            <img
+              src={bustShotMobile}
+              alt="Peter Lofland bust shot mobile"
+            />
+          ) : (
+            <img
+              src={bustShot}
+              alt="Peter Lofland bust shot"
+            />
+          )}
         </div>
       </div>
     </div>
