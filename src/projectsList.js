28949b5c0ad0315934a62b921a80966f4625a33d
Plofland <ploflan@gmail.com>
2021-05-30 03:16:51 -0400

    added descriptions to projects

--- src/projectsList.js
+++ src/projectsList.js
@@ -1,4 +1,4 @@
-import hkLanding from './assets/product-preview.png';
+// import hkLanding from './assets/product-preview.png';
 import todoApp from './assets/todoReactApp.png';
 import coMake from './assets/co-make.png';
 import LAN from './assets/LAN.jpg';
@@ -8,6 +8,7 @@ const projectsList = [
     id: 0,
     projectName: 'Lambda Alumni Association',
     hrefLink: 'https://a.lambdaalumninetwork.dev/welcome',
+    description: 'LAN unified, private community for Lambda school alumni to connect, advance their careers, and give back. ',
     imgSrc: LAN,
     imgAlt: 'Lambda Alumni Association project'
   },
@@ -15,6 +16,7 @@ const projectsList = [
     id: 1,
     projectName: 'Co-Make',
     hrefLink: 'https://co-make-tt33.herokuapp.com/',
+    description: 'Have your voice heard on the issues you would like to see resolved in your community',
     imgSrc: coMake,
     imgAlt: 'Co-Make project'
   },
@@ -22,16 +24,17 @@ const projectsList = [
     id: 2,
     projectName: 'ToDo Application',
     hrefLink: 'https://my-very-own-todo-app.netlify.app/',
+    description: 'Create and edit a list of Todo\'s',
     imgSrc: todoApp,
     imgAlt: 'ToDo React App'
   },
-  {
-    id: 3,
-    projectName: 'Product Landing Page',
-    hrefLink: 'https://hkproductlandingpage.netlify.app/',
-    imgSrc: hkLanding,
-    imgAlt: 'Product landing page project'
-  },
+  // {
+  //   id: 3,
+  //   projectName: 'Product Landing Page',
+  //   hrefLink: 'https://hkproductlandingpage.netlify.app/',
+  //   imgSrc: hkLanding,
+  //   imgAlt: 'Product landing page project'
+  // },
 ];
 
 export default projectsList;
