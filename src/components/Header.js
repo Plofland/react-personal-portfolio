c058a50accebc4cd9c464a16746970c8d9a5cfc7
Plofland <ploflan@gmail.com>
2021-06-09 20:20:19 -0400

    Animation added to header banner img

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,37 +6,37 @@ import monstera900 from '../assets/monsteraBackground900.jpg';
 import monstera414 from '../assets/monsteraBackground414.jpg';
 
 export default function Header() {
-  return (
-    <div className="headerSection">
-      {window.innerWidth <= 414 ? (
-        <img
-          src={monstera414}
-          alt="monstera plant banner mobile"
-        />
-      ) : window.innerWidth <= 900 ? (
-        <img
-          src={monstera900}
-          alt="monstera plant banner tablet"
-        />
-      ) : window.innerWidth <= 1440 ? (
-        <img
-          src={monstera1440}
-          alt="monstera plant banner Macbook"
-        />
-      ) : (
-        <img
-          src={monstera}
-          alt="monstera plant banner desktop"
-        />
-      )}
+	return (
+		<div className="headerSection">
+			{window.innerWidth <= 414 ? (
+				<img
+					src={monstera414}
+					alt="monstera plant banner mobile"
+				/>
+			) : window.innerWidth <= 900 ? (
+				<img
+					src={monstera900}
+					alt="monstera plant banner tablet"
+				/>
+			) : window.innerWidth <= 1440 ? (
+				<img
+					src={monstera1440}
+					alt="monstera plant banner Macbook"
+				/>
+			) : (
+				<img
+					src={monstera}
+					alt="monstera plant banner desktop"
+				/>
+			)}
 
-      <a
-        href="https://www.linkedin.com/in/peter-lofland/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <h1>Peter Lofland</h1>
-      </a>
-    </div>
-  );
+			<a
+				href="https://www.linkedin.com/in/peter-lofland/"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<h1>Peter Lofland</h1>
+			</a>
+		</div>
+	);
 }
